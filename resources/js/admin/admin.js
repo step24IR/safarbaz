fbd62d2146fec673a1f27adf5dc2240a29a72407
hoosein nateghi <hooseinnateghi7198@gmail.com>
2024-03-30 23:04:58 +0330

    fix error 3

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -376,14 +376,7 @@ Title: sidebar
 */
 
 (function webpackUniversalModuleDefinition(root, factory) {
-    if(typeof exports === 'object' && typeof module === 'object')
-        module.exports = factory();
-    else if(typeof define === 'function' && define.amd)
-        define([], factory);
-    else if(typeof exports === 'object')
-        exports["persianDatepicker"] = factory();
-    else
-        root["persianDatepicker"] = factory();
+    root = factory();
 })(this, function() {
     return /******/ (function(modules) { // webpackBootstrap
         /******/ 	// The module cache
