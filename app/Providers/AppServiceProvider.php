a7814d45a1959991aa6d70e4a014a67ddbf0ad39
hoosein nateghi <hooseinnateghi7198@gmail.com>
2024-03-30 15:32:55 +0330

    update app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Paginator::defaultView('admin.sections.pagination');
     }
 }
